e517838c5bf636f3d734d31dc55230a190c5e1c4
Bal Mukund <bmukund@nuodb.com>
2014-10-10 13:36:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
-
-class Hello{
-
-public static void main(String arg[]){
-
-System.out.println("Hello ! World");
-
-  }
+public class Main {
+ 
+    public static void main(String [] args) {
+        System.out.println("Hello ! World");
+    }
 }
